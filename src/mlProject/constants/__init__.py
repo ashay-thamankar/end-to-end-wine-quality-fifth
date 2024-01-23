9357e598f7e9473e44099f38c45f7c2d788631e8
ashay-thamankar <ashaythamankar@gmail.com>
2024-01-23 12:23:20 +0530

    hyper parameter tuning

--- src/mlProject/constants/__init__.py
+++ src/mlProject/constants/__init__.py
@@ -3,3 +3,4 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path('config/config.yaml')
 PARAMS_FILE_PATH = Path('params.yaml')
 SCHEME_FILE_PATH = Path('schema.yaml')
+MODEL_P_FILE_PATH = Path('model_params.yaml')
